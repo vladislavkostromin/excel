080d55ce42ffac2ad175c5b98d630806e397d502
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-01-08 17:49:25 +0500

    добавил классы

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import {
+  DomListener
+} from './DomListener';
+
+export class ExcelComponent extends DomListener {
+  toHTML() {
+    return ''
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+import {Excel} from './components/excel/Excel'
 import './scss/index.scss'
+
+const excel = new Excel('#app', {
+  components: []
+})
+
 
