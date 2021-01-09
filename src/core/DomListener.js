20b320c1004257979b491d876b11be1e114185e5
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-01-09 22:42:53 +0500

    структура событий

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,13 @@
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error(`No $root providet for DomListener`)
     }
     this.$root = $root
+    this.listeners = listeners
   }
+
+  initDOMListeners() {}
+
+  removeDOMListeners() {}
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,7 +3,15 @@ import {
 } from './DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+  }
+
   toHTML() {
     return ''
+  }
+
+  init() {
+    this.initDOMListeners()
   }
 }
